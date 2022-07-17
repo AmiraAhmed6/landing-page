e32f4e447811d8d1c80f21671cd4e7eda41c736a
AmiraAhmed6 <77937175+AmiraAhmed6@users.noreply.github.com>
2022-07-18 01:38:05 +0200

    v

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
+//variables
 const mainUl = document.querySelector('#navbar__list');
 const sections = document.querySelectorAll('section');
+// const mediaQuery = window.matchMedia("(max-width:700px)");
 
 // Navbar li
 
 for(let i=0 ; i < sections.length ; i++){
-    liNav = document.createElement('li');
+   const liNav = document.createElement('li');
     mainUl.appendChild(liNav);
     liNav.textContent = sections.item(i).getAttribute("data-nav");
+
 }
+
