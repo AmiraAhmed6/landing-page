df4de2819332aaa3a4a0aefeac276162a3342e1a
AmiraAhmed6 <77937175+AmiraAhmed6@users.noreply.github.com>
2022-07-18 00:16:55 +0200

    nn

--- js/app.js
+++ js/app.js
@@ -1,20 +1,10 @@
 const mainUl = document.querySelector('#navbar__list');
-//first Nav Bar li
-const liNav1 = document.createElement('li');
-mainUl.appendChild(liNav1);
-liNav1.textContent = "Section 1";
+const sections = document.querySelectorAll('section');
 
-// second Nav Bar li
-const liNav2 = document.createElement('li');
-mainUl.appendChild(liNav2);
-liNav2.textContent = "Section 2";
+// Navbar li
 
-// third Nav Bar li
-const liNav3 = document.createElement('li');
-mainUl.appendChild(liNav3);
-liNav3.textContent = "Section 3";
-
-// fourth Nav Bar li
-const liNav4 = document.createElement('li');
-mainUl.appendChild(liNav4);
-liNav4.textContent = "Section 4";
+for(let i=0 ; i < sections.length ; i++){
+    liNav = document.createElement('li');
+    mainUl.appendChild(liNav);
+    liNav.textContent = sections.item(i).getAttribute("data-nav");
+}
